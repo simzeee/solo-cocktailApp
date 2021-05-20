e539595295549c0bcd2e2525000e4f7bfd648526
John Sims <jwsims816@gmail.com>
2021-05-20 13:02:52 -0500

    navlinks and profile icon

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 
+import './SignupForm.css';
+
+
 function SignupFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
