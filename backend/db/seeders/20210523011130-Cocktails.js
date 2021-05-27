956e08aca6c48f177bc87e7c7bd76a960366b178
John Sims <jwsims816@gmail.com>
2021-05-27 17:01:55 -0500

    added some seeders

--- backend/db/seeders/20210523011130-Cocktails.js
+++ backend/db/seeders/20210523011130-Cocktails.js
@@ -14,6 +14,43 @@ module.exports = {
           commentId: 1,
           classic: true,
         },
+        {
+          name: 'Martini',
+          description: 'Stronger than you think.',
+          imageUrl:
+            'https://www.liquor.com/thmb/Tg83gGIz-cS8nO-aAHa2N6R4eFQ=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/dry-martini-720x720-primary-a6de08f8cd584ad88520287922578bcb.jpg',
+          userId: 1,
+          commentId: 1,
+          classic: true,
+        },
+        {
+          name: 'Old Fashioned',
+          description: 'Classic and delicious',
+          imageUrl:
+            'https://images.absolutdrinks.com/drink-images/Raw/Absolut/01723f0d-1c38-4955-b75d-c9b822a20c7a.jpg?imwidth=500',
+          userId: 1,
+          commentId: 1,
+          classic: true,
+        },
+        {
+          name: 'Martinez',
+          description: 'Boozy.',
+          imageUrl:
+            'https://www.acouplecooks.com/wp-content/uploads/2020/05/Martinez-Cocktail-021.jpg',
+          userId: 1,
+          commentId: 1,
+          classic: true,
+        },
+        {
+          name: 'Whiskey Sour',
+          description: 'Foamy, frothy, good',
+          imageUrl:
+            'https://www.acouplecooks.com/wp-content/uploads/2020/05/Martinez-Cocktail-021.jpg',
+          userId: 1,
+          commentId: 1,
+          classic: true,
+        },
+        
       ],
       {}
     );
